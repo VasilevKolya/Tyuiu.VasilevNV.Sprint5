5505e409edf3f3ab52c2ddf14e693580af1cde7d
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-14 20:44:18 +0400

    Task0

--- Tyuiu.VasilevNV.Sprint5.Task0.V20.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task0.V20.Lib/DataService.cs
@@ -5,10 +5,10 @@ namespace Tyuiu.VasilevNV.Sprint5.Task0.V20.Lib
 {
     public class DataService : ISprint5Task0V20
     {
-        public string SaveToFileTextData(int x)
+        public  string SaveToFileTextData(int x)
         {
             double res;
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             res = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2;
             res = Math.Round(res, 3);
             File.WriteAllText(path, Convert.ToString(res));
