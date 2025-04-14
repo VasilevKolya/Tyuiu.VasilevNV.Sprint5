8aa52b2a5fd9e06ac290adda70095fa71b2f2e1a
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-14 20:40:44 +0400

    Add project files.

---
+++ Tyuiu.VasilevNV.Sprint5.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.VasilevNV.Sprint5.Task0.V20.Lib
+{
+    public class DataService : ISprint5Task0V20
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double res;
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            res = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2;
+            res = Math.Round(res, 3);
+            File.WriteAllText(path, Convert.ToString(res));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.VasilevNV.Sprint5.Task0.V20/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.VasilevNV.Sprint5.Task0.V20.Lib;
+using System;
+using System.Text;
+namespace Tyuiu.VasilevNV.Sprint5.Task0.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+int x = 2;
+Console.WriteLine("*Спринт #5 | Выполнил: Васильев Н. В. | ИСПБ-24-1                       *");
+Console.WriteLine("*************************************************************************");
+Console.WriteLine("* Спринт #5                                                             *");
+Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                      *");
+Console.WriteLine("* Задание #0                                                            *");
+Console.WriteLine("* Вариант #20                                                           *");
+Console.WriteLine("*  Выполнил: Васильев Н. В. | ИСПБ-24-1                                 *");
+Console.WriteLine("*************************************************************************");
+Console.WriteLine("*УСЛОВИЕ:                                                               *");
+Console.WriteLine("Дано выражение , вычислить его значение при x = 2, результат сохранить   *");
+Console.WriteLine("в текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх знаков после запятой. *");
+Console.WriteLine("*                                                                       *");
+Console.WriteLine("*************************************************************************");
+Console.WriteLine(" ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+Console.WriteLine("*************************************************************************");
+Console.WriteLine("X = " + x);
+DataService ds = new DataService();
+Console.WriteLine("*************************************************************************");
+Console.WriteLine(" РЕЗУЛЬТАТ:                                                             *");
+Console.WriteLine("*************************************************************************");
+
+       string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
