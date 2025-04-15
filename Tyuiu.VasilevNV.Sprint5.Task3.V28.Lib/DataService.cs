6db671cf593a39fd2bd589da5ea820a96d32a37d
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-15 18:14:26 +0400

    Task3

--- Tyuiu.VasilevNV.Sprint5.Task3.V28.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task3.V28.Lib/DataService.cs
@@ -8,14 +8,26 @@ namespace Tyuiu.VasilevNV.Sprint5.Task3.V28.Lib
     {
         public string SaveToFileTextData(int x)
         {
-           string path =$@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
             double res = -0.25 * (Math.Pow(x, 3) - 3 * Math.Pow(x, 2) + 4);
-            res = Math.Round(res,3);
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            res = Math.Round(res, 3);
+
+          
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            try
             {
-               writer.Write(BitConverter.GetBytes(res));
+              
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+                {
+                    writer.Write(res);
+                }
+                return path;
             }
-            return path;
+            catch (Exception ex)
+            {
+            
+                return $"Ошибка при сохранении файла: {ex.Message}";
+            }
         }
     }
 }
