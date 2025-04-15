690d18282093f07ea07d06ba49c827a2aff3ecba
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-15 17:04:04 +0400

    Task1

--- Tyuiu.VasilevNV.Sprint5.Task1.V15.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task1.V15.Lib/DataService.cs
@@ -19,6 +19,7 @@ namespace Tyuiu.VasilevNV.Sprint5.Task1.V15.Lib
                 double part1 = Math.Cos(x) / (x - 0.4);
                 double part2 = Math.Sin(x) * 8 * x;
                 double result = part1 + part2 + 2;
+                result = Math.Round(result, 2);
                 strY = Convert.ToString(result);
                 if (x != stopValue)
                 {
