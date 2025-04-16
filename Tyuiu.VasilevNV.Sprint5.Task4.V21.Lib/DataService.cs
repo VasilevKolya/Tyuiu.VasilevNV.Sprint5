72303b0f84277633a35c524d167e26ceebdb710b
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-16 12:05:56 +0400

    Task4

--- Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib/DataService.cs
@@ -1,5 +1,6 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
+using System.Globalization;
 namespace Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib
 {
     public class DataService : ISprint5Task4V21
@@ -7,6 +8,7 @@ namespace Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
+            if (double.TryParse(strX, NumberStyles.Any, CultureInfo.InvariantCulture, out double x)) ;
             double res = Math.Round(Math.Pow(Convert.ToDouble(strX), 2) + 3 * (Convert.ToDouble(strX)) + 5, 3);
             return res;
         }
