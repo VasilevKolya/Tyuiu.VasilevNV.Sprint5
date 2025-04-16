f598bf552a303f3e30b0cd1f28c03fbdccfc8498
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-16 12:07:53 +0400

    Task4

--- Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path);
+            string strX = File.ReadAllText(path).Trim();
             if (double.TryParse(strX, NumberStyles.Any, CultureInfo.InvariantCulture, out double x)) ;
             double res = Math.Round(Math.Pow(Convert.ToDouble(strX), 2) + 3 * (Convert.ToDouble(strX)) + 5, 3);
             return res;
--- Tyuiu.VasilevNV.Sprint5.Task4.V21.Test/DataServiceTest.cs
+++ Tyuiu.VasilevNV.Sprint5.Task4.V21.Test/DataServiceTest.cs
@@ -9,10 +9,16 @@ namespace Tyuiu.VasilevNV.Sprint5.Task4.V21.Test
         public void TestMethod1()
         {
             string path = @"C:\Users\nvasi\source\repos\Tyuiu.VasilevNV.Sprint5\Tyuiu.VasilevNV.Sprint5.Task4.V21\bin\Debug\InPutDataFileTask4V21.txt";
-            FileInfo fileinfo = new FileInfo(path);
-            bool fileExits = fileinfo.Exists;
-            bool wait = true;
-            Assert.AreEqual(wait, fileExits);
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+
+            DataService ds = new DataService();
+            double res = ds.LoadFromDataFile(path);
+            double wait = 4.68 * 4.68 + 3 * 4.68 + 5; 
+            wait = Math.Round(wait, 3);
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
