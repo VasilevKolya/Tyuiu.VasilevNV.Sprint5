4ccace42537bce822dc2a7de5d6bcafe1c5d9f45
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-16 20:13:34 +0400

    Task4

--- Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib/DataService.cs
@@ -9,16 +9,13 @@ namespace Tyuiu.VasilevNV.Sprint5.Task4.V21.Lib
         {
             string strX = File.ReadAllText(path).Trim();
 
-            NumberFormatInfo provider = new NumberFormatInfo
-            {
-                NumberDecimalSeparator = ".",
-                NumberGroupSeparator = ""
-            };
 
-            double x = double.Parse(strX, provider);
+            double x = double.Parse(strX, CultureInfo.InvariantCulture);
+
+            double result = Math.Pow(x, 3) * Math.Cos(x) + 2 * x;
 
 
-            return Math.Round(Math.Pow(x, 3) + 2 * x / Math.Cos(x), 3);
+            return Math.Round(result, 3);
         }
     }
 }
