185bab9ee235da34d439e8ad2ef4b8d20fd5882b
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-17 19:34:42 +0400

    Task5

--- Tyuiu.VasilevNV.Sprint5.Task5.V1.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task5.V1.Lib/DataService.cs
@@ -10,24 +10,27 @@ namespace Tyuiu.VasilevNV.Sprint5.Task5.V1.Lib
             string[] lines = File.ReadAllLines(path);
 
             double sum = 0;
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+            provider.NumberGroupSeparator = "";
 
             foreach (string line in lines)
             {
                
                 if (string.IsNullOrWhiteSpace(line)) continue;
 
-                
-                if (double.TryParse(line, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+               
+                string cleanLine = line.Trim().Replace(",", ".");
+
+                if (double.TryParse(cleanLine, NumberStyles.Any, provider, out double number))
                 {
-                   
-                    if (Math.Abs(number % 2) < double.Epsilon)
+                    if (number % 2 == 0)
                     {
                         sum += number;
                     }
                 }
             }
 
-           
             return Math.Round(sum, 3);
         }
     }
