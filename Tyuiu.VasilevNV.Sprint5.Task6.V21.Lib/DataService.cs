17218613ea6c37a147bec28b942b86ca05453244
Николай Васильев <nvasiljev1977@gmail.com>
2025-04-28 20:55:54 +0400

    Task6

--- Tyuiu.VasilevNV.Sprint5.Task6.V21.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint5.Task6.V21.Lib/DataService.cs
@@ -1,23 +1,31 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
+using System.Text;
 namespace Tyuiu.VasilevNV.Sprint5.Task6.V21.Lib
 {
     public class DataService : ISprint5Task6V21
     {
         public int LoadFromDataFile(string path)
         {
-            string content = File.ReadAllText(path);
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Файл не найден.", path);
+            }
+
+            string content = File.ReadAllText(path, Encoding.UTF8);  
             int count = 0;
 
             for (int i = 0; i < content.Length - 1; i++)
             {
+
                 if (content[i] == 'т' && content[i + 1] == 'т')
                 {
                     count++;
-
                 }
             }
+
             return count;
         }
     }
-} 
+}
